c3ac4d45a9a75fafd97639df8687d0eca134623a
Faheem Ahmed Noor <faheem.ahmad@euphoria.pk>
2022-04-05 15:15:31 +0500

    Implement Basic Login

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-
-  get '/home', to: 'users#home'
   # get 'users/home'
+  get '/home', to: 'users#home'
 
-  get '/login', to: 'users#login'
   # get 'users/login'
+  # get '/login', to: 'users#login'
 
-  get '/signup', to: 'users#signup'
   # get 'users/signup'
+  # get '/signup', to: 'users#signup'
+  get '/signup', to: 'users#new'
+
 
   root 'users#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
@@ -17,4 +18,5 @@ Rails.application.routes.draw do
   get '/login', to:'sessions#new'
   post '/login',to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  resources :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_30_104202) do
+ActiveRecord::Schema.define(version: 2022_03_31_144709) do
 
   create_table "bugs", force: :cascade do |t|
     t.string "title"
@@ -24,9 +24,9 @@ ActiveRecord::Schema.define(version: 2022_03_30_104202) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
